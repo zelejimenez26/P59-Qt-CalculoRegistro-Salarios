bcb00c9e22ab3f7dd18fb4fdc61f258964d36597
Zelena Jimenez <zjimenezh@est.ups.edu.ec>
2022-01-16 23:51:36 -0600

    Calculos desde la GUI

--- salarios.cpp
+++ salarios.cpp
@@ -6,10 +6,35 @@ Salarios::Salarios(QWidget *parent)
     , ui(new Ui::Salarios)
 {
     ui->setupUi(this);
+    m_controlador=new Controlador(); //Intanciar objeto
 }
 
 Salarios::~Salarios()
 {
     delete ui;
+}
+
+
+void Salarios::on_cmdCalcular_clicked()
+{
+    //Obtener datos de la GUI
+    QString nombre=ui->inNombre->text(); //texto del widget
+    int horas=ui->inHoras->value(); //value(), devuleve solo el valor
+    tipoJornada jornada;
+    if(ui->inMatutino->isChecked()){
+        jornada=tipoJornada::Matutina;
+    }else if(ui->inVespertina->isChecked()){
+        jornada=tipoJornada::Vespertina;
+    }else{
+        jornada=tipoJornada::Vespertina;
+    }
+    //Agregar obrero al controlador
+    m_controlador->agregarObrero(nombre, horas, jornada);
+    //Calcular
+    if(m_controlador->calcularSalario()){
+        ui->outResultado->appendPlainText(m_controlador->obrero()->toString());
+    }
+
+
 }
 
--- salarios.h
+++ salarios.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+#include "controlador.h"
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class Salarios; }
 QT_END_NAMESPACE
@@ -15,7 +17,11 @@ public:
     Salarios(QWidget *parent = nullptr);
     ~Salarios();
 
+private slots:
+    void on_cmdCalcular_clicked();
+
 private:
     Ui::Salarios *ui;
+    Controlador *m_controlador;
 };
 #endif // SALARIOS_H
